d973a13b115beb77d8d337d8d203f75c9f3f4fd8
lwl <69039958@qq.com>
2020-04-23 10:48:27 +0800

    TokenFiler成功

--- gateway-service-zuul/gateway-service-zuul-eureka/src/main/java/com/neo/GatewayServiceZuulApplication.java
+++ gateway-service-zuul/gateway-service-zuul-eureka/src/main/java/com/neo/GatewayServiceZuulApplication.java
@@ -3,10 +3,17 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.neo.filter.TokenFilter;
+
 
 @SpringBootApplication
 @EnableZuulProxy
 public class GatewayServiceZuulApplication {
+	@Bean
+	public TokenFilter tokenFilter() {
+		return new TokenFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceZuulApplication.class, args);
