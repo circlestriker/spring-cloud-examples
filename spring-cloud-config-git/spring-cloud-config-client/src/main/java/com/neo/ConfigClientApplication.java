d0d3e5a2ae10281c1c3f64c426226db9f001dcbd
lwl <69039958@qq.com>
2020-04-23 16:44:30 +0800

    自动refresh配置

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
