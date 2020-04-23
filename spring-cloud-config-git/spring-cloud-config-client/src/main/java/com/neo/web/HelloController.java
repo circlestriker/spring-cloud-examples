5750a52b572811b912cad1bcba6d85b2c2a32b85
lwl <69039958@qq.com>
2020-04-23 16:22:38 +0800

    neo.hello读取配置

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/web/HelloController.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class HelloController {
 
-    @Value("${neo.hello}")
+    @Value("${neo.hello}") //从这个配置读取的值: neo.hello=hello im dev update 2
     private String hello;
 
     @RequestMapping("/hello")
