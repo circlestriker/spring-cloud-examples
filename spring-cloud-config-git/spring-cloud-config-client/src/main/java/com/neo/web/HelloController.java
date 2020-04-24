dbe4d0cbee9b2f18b55e70efca79521bfb2d0c8e
lwl <69039958@qq.com>
2020-04-24 15:52:28 +0800

    @RefreshScope  //之前这里少了这个注解导致未更新

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/web/HelloController.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/web/HelloController.java
@@ -3,8 +3,10 @@ package com.neo.web;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RestController
+@RefreshScope  //之前这里少了这个注解导致未更新
 class HelloController {
 
     @Value("${neo.hello}") //从这个配置读取的值: neo.hello=hello im dev update 2
