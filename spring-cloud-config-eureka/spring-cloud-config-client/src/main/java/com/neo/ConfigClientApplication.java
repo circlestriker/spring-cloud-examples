de6f80f848f41273e88dce755ca94e4feb0c5165
lwl <69039958@qq.com>
2020-04-24 09:46:38 +0800

    解决 ch/qos/logback/classic/spi/ThrowableProxy 错误。 不要在同 个目录起2个服务

--- spring-cloud-config-eureka/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-eureka/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
@@ -4,9 +4,11 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
